e4bdcd6748aad22d99c62f64993d9ba7200ae0d9
Philip <philip.arvidsson97@gmail.com>
2024-09-24 23:54:39 +0200

    Added about and contact with profilepic

--- src/Modules/Meny.js
+++ src/Modules/Meny.js
@@ -1,13 +1,12 @@
 import '../Style.css'
-import insta from '../Pictures/Instagram_icon.png'
+import insta from '../Logo/whiteInstaLogo.png'
 
 function Meny() {
 	return (    
 		<header className="meny">
-			<div className="flex">
-				<div className="center w100"><h3 className="menuTitle" >Gaz Pictures</h3></div>
+			<div className="">
+				<div className="t-align-start t-white"><h3 className="menuTitle" >Gaz Pictures</h3></div>
 				<div className="padding-right pAbsolute flex">
-					<img src={insta} className="icon" alt="instagram logo" />
 				</div>
 			</div>
 		</header>
