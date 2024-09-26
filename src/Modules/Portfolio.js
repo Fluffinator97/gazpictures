d3327c18e2c0d3b89e98dde03e672ff1b6d7b13e
Philip <philip.arvidsson97@gmail.com>
2024-09-26 04:22:22 +0200

    couldnt sleep so i added a lightbox and auto img import

--- src/Modules/Portfolio.js
+++ src/Modules/Portfolio.js
@@ -1,99 +1,71 @@
-import '../Style.css'
+import '../Style.css';
+import '../imgModal.css';
 
-import a from "../Photos/1DSCF2102.jpg";
-import b from "../Photos/1DSCF2118.jpg";
-import c from "../Photos/2DSCF2098.jpg";
-import e from "../Photos/4DSCF2140.jpg";
-import f from "../Photos/5DSCF2086.jpg";
-import g from "../Photos/DSCF1464-min.jpg";
-import h from "../Photos/DSCF1477-min.jpg";
-import i from "../Photos/DSCF1490-min.jpg";
-import j from "../Photos/DSCF1493-min.jpg";
-import l from "../Photos/DSCF1496-min.jpg";
-import m from "../Photos/DSCF2126.jpg";
-import n from "../Photos/DSCF2128.jpg";
-import o from "../Photos/DSCF2260-Enhanced-NR-min.jpg";
-import p from "../Photos/DSCF2262-Enhanced-NR-min.jpg";
-import q from "../Photos/DSCF2279-min.jpg";
-import r from "../Photos/DSCF2282-min.jpg";
-import s from "../Photos/DSCF2323-min.jpg";
-import t from "../Photos/DSCF2329-min.jpg";
-import u from "../Photos/DSCF2330-min.jpg";
-import v from "../Photos/DSCF2331-min.jpg";
-import x from "../Photos/DSCF2334-Enhanced-NR-min.jpg";
-import y from "../Photos/DSCF2339-min.jpg";
-import z from "../Photos/DSCF4429.jpg";
-import aa from "../Photos/DSCF4497.jpg";
-import ab from "../Photos/DSCF4506.jpg";
-import ac from "../Photos/DSCF4511.jpg";
-import ad from "../Photos/DSCF4542.jpg";
-import ae from "../Photos/DSCF4572.jpg";
-import af from "../Photos/DSCF4610.jpg";
-import ag from "../Photos/DSCF4616.jpg";
-import ah from "../Photos/DSCF5093-min.jpg";
-import ai from "../Photos/DSCF5173-min.jpg";
-import aj from "../Photos/DSCF2241.jpg";
-import ak from "../Photos/DSCF2202.jpg";
-import al from "../Photos/DSCF2243.jpg";
-import am from "../Photos/DSCF2240.jpg";
-import an from "../Photos/DSCF2184-Enhanced-NR.jpg";
-import ao from "../Photos/1DSCF2204.jpg";
-import ap from "../Photos/DSCF5173-min.jpg";
-import aq from "../Photos/DSCF2175.jpg";
-import ar from "../Photos/DSCF2238.jpg";
-import as from "../Photos/1DSCF2179.jpg";
-import at from "../Photos/DSCF2192.jpg";
-import au from "../Photos/DSCF2236.jpg";
-import av from "../Photos/DSCF2195.jpg";
+import { useState } from 'react';
 
-function Portfolio() {
+const images = require.context('../photos', true);
+const imageList = images.keys().map(image => images(image));
+function Portfolio(props) {
+
+
+const images = require.context('../photos', true);
+const imageList = images.keys().map(image => images(image));
+const [imageToShow, setImageToShow] = useState("");
+const [lightboxDisplay, setLightBoxDisplay] = useState(false);
+
+
+
+const showImage = (image) => {
+    setImageToShow(image);
+    setLightBoxDisplay(true);
+	document.body.classList.add("remove-scrolling"); 
+  };
+
+  //hide lightbox
+  const hideLightBox = () => {
+    setLightBoxDisplay(false);
+	document.body.classList.remove("remove-scrolling"); 
+  };
+
+  //show next image in lightbox
+  const showNext = (e) => {
+    e.stopPropagation();
+    let currentIndex = imageList.indexOf(imageToShow);
+    if (currentIndex >= imageList.length - 1) {
+      setLightBoxDisplay(false);
+    } else {
+      let nextImage = imageList[currentIndex + 1];
+      setImageToShow(nextImage);
+    }
+  };
+
+
+  //show previous image in lightbox
+  const showPrev = (e) => {
+    e.stopPropagation();
+    let currentIndex = imageList.indexOf(imageToShow);
+    if (currentIndex <= 0) {
+      setLightBoxDisplay(false);
+    } else {
+      let nextImage = imageList[currentIndex - 1];
+      setImageToShow(nextImage);
+    }
+  };
 
 return (
 	<div className="" id="images-wrapper">
-			<img src={a} sizes="(min-width: 960px) 33vw, (min-width: 640px) 50vw, 100vw" width="600" className="flex-auto" alt="instagram logo" />
-			<img src={b} sizes="(min-width: 960px) 33vw, (min-width: 640px) 50vw, 100vw" width="600" className="flex-auto" alt="instagram logo" />
-			<img src={c} sizes="(min-width: 960px) 33vw, (min-width: 640px) 50vw, 100vw" width="600" className="flex-auto" alt="instagram logo" />
-			<img src={e} sizes="(min-width: 960px) 33vw, (min-width: 640px) 50vw, 100vw" width="600" className="flex-auto" alt="instagram logo" />
-			<img src={f} sizes="(min-width: 960px) 33vw, (min-width: 640px) 50vw, 100vw" width="600" className="flex-auto" alt="instagram logo" />
-			<img src={g} sizes="(min-width: 960px) 33vw, (min-width: 640px) 50vw, 100vw" width="600" className="flex-auto" alt="instagram logo" />
-			<img src={h} sizes="(min-width: 960px) 33vw, (min-width: 640px) 50vw, 100vw" width="600" className="flex-auto" alt="instagram logo" />
-			<img src={i} sizes="(min-width: 960px) 33vw, (min-width: 640px) 50vw, 100vw" width="600" className="flex-auto" alt="instagram logo" />
-			<img src={j} sizes="(min-width: 960px) 33vw, (min-width: 640px) 50vw, 100vw" width="600" className="flex-auto" alt="instagram logo" />
-			<img src={l} sizes="(min-width: 960px) 33vw, (min-width: 640px) 50vw, 100vw" width="600" className="flex-auto" alt="instagram logo" />
-			<img src={m} sizes="(min-width: 960px) 33vw, (min-width: 640px) 50vw, 100vw" width="600" className="flex-auto" alt="instagram logo" />
-			<img src={n} sizes="(min-width: 960px) 33vw, (min-width: 640px) 50vw, 100vw" width="600" className="flex-auto" alt="instagram logo" />
-			<img src={o} sizes="(min-width: 960px) 33vw, (min-width: 640px) 50vw, 100vw" width="600" className="flex-auto" alt="instagram logo" />
-			<img src={p} sizes="(min-width: 960px) 33vw, (min-width: 640px) 50vw, 100vw" width="600" className="flex-auto" alt="instagram logo" />
-			<img src={q} sizes="(min-width: 960px) 33vw, (min-width: 640px) 50vw, 100vw" width="600" className="flex-auto" alt="instagram logo" />
-			<img src={r} sizes="(min-width: 960px) 33vw, (min-width: 640px) 50vw, 100vw" width="600" className="flex-auto" alt="instagram logo" />
-			<img src={s} sizes="(min-width: 960px) 33vw, (min-width: 640px) 50vw, 100vw" width="600" className="flex-auto" alt="instagram logo" />
-			<img src={t} sizes="(min-width: 960px) 33vw, (min-width: 640px) 50vw, 100vw" width="600" className="flex-auto" alt="instagram logo" />
-			<img src={u} sizes="(min-width: 960px) 33vw, (min-width: 640px) 50vw, 100vw" width="600" className="flex-auto" alt="instagram logo" />
-			<img src={v} sizes="(min-width: 960px) 33vw, (min-width: 640px) 50vw, 100vw" width="600" className="flex-auto" alt="instagram logo" />
-			<img src={x} sizes="(min-width: 960px) 33vw, (min-width: 640px) 50vw, 100vw" width="600" className="flex-auto" alt="instagram logo" />
-			<img src={y} sizes="(min-width: 960px) 33vw, (min-width: 640px) 50vw, 100vw" width="600" className="flex-auto" alt="instagram logo" />
-			<img src={z} sizes="(min-width: 960px) 33vw, (min-width: 640px) 50vw, 100vw" width="600" className="flex-auto" alt="instagram logo" />
-			<img src={aa} sizes="(min-width: 960px) 33vw, (min-width: 640px) 50vw, 100vw" width="600" className="flex-auto" alt="instagram logo" />
-			<img src={ab} sizes="(min-width: 960px) 33vw, (min-width: 640px) 50vw, 100vw" width="600" className="flex-auto" alt="instagram logo" />
-			<img src={ac} sizes="(min-width: 960px) 33vw, (min-width: 640px) 50vw, 100vw" width="600" className="flex-auto" alt="instagram logo" />
-			<img src={ad} sizes="(min-width: 960px) 33vw, (min-width: 640px) 50vw, 100vw" width="600" className="flex-auto" alt="instagram logo" />
-			<img src={ae} sizes="(min-width: 960px) 33vw, (min-width: 640px) 50vw, 100vw" width="600" className="flex-auto" alt="instagram logo" />
-			<img src={af} sizes="(min-width: 960px) 33vw, (min-width: 640px) 50vw, 100vw" width="600" className="flex-auto" alt="instagram logo" />
-			<img src={ag} sizes="(min-width: 960px) 33vw, (min-width: 640px) 50vw, 100vw" width="600" className="flex-auto" alt="instagram logo" />
-			<img src={ah} sizes="(min-width: 960px) 33vw, (min-width: 640px) 50vw, 100vw" width="600" className="flex-auto" alt="instagram logo" />
-			<img src={ai} sizes="(min-width: 960px) 33vw, (min-width: 640px) 50vw, 100vw" width="600" className="flex-auto" alt="instagram logo" />
-			<img src={aj} sizes="(min-width: 960px) 33vw, (min-width: 640px) 50vw, 100vw" width="600" className="flex-auto" alt="instagram logo" />
-			<img src={ak} sizes="(min-width: 960px) 33vw, (min-width: 640px) 50vw, 100vw" width="600" className="flex-auto" alt="instagram logo" />
-			<img src={al} sizes="(min-width: 960px) 33vw, (min-width: 640px) 50vw, 100vw" width="600" className="flex-auto" alt="instagram logo" />
-			<img src={am} sizes="(min-width: 960px) 33vw, (min-width: 640px) 50vw, 100vw" width="600" className="flex-auto" alt="instagram logo" />
-			<img src={an} sizes="(min-width: 960px) 33vw, (min-width: 640px) 50vw, 100vw" width="600" className="flex-auto" alt="instagram logo" />
-			<img src={ao} sizes="(min-width: 960px) 33vw, (min-width: 640px) 50vw, 100vw" width="600" className="flex-auto" alt="instagram logo" />
-			<img src={ap} sizes="(min-width: 960px) 33vw, (min-width: 640px) 50vw, 100vw" width="600" className="flex-auto" alt="instagram logo" />
-			<img src={ar} sizes="(min-width: 960px) 33vw, (min-width: 640px) 50vw, 100vw" width="600" className="flex-auto" alt="instagram logo" />
-			<img src={as} sizes="(min-width: 960px) 33vw, (min-width: 640px) 50vw, 100vw" width="600" className="flex-auto" alt="instagram logo" />
-			<img src={at} sizes="(min-width: 960px) 33vw, (min-width: 640px) 50vw, 100vw" width="600" className="flex-auto" alt="instagram logo" />
-			<img src={au} sizes="(min-width: 960px) 33vw, (min-width: 640px) 50vw, 100vw" width="600" className="flex-auto" alt="instagram logo" />
-			<img src={av} sizes="(min-width: 960px) 33vw, (min-width: 640px) 50vw, 100vw" width="600" className="flex-auto" alt="instagram logo" />
+		{imageList.map((image, index) => (
+				<img key={index} src={image} onClick={() => showImage(image)} sizes="(min-width: 960px) 33vw, (min-width: 640px) 50vw, 100vw" width="30%" className="flex-auto image-card" alt={`image-${index}`} />
+			))}
+	{
+        lightboxDisplay ? 
+        <div id="lightbox" onClick={hideLightBox}>
+          <button className="lightboxButton" onClick={showPrev}>&lt;</button>
+          <img id="lightbox-img" src={imageToShow} alt="a"></img>
+          <button className="lightboxButton" onClick={showNext}>&gt;</button>
+        </div>
+       : ""
+      }
 	</div>
 );
 }
